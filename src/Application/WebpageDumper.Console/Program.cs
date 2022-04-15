5b334c3c0669c43d977edcddd5c301c30ed31b18
Julia Mattjus <julia.mattjus@sigma.se>
2022-04-15 13:05:40 +0200

    Initial project setup and skeleton

---
+++ src/Application/WebpageDumper.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace WebpageDumper.Application.WebpageDumper.Console;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).ConfigureAppConfiguration((hostContext, builder) =>
+        { }).Build();
+        await host.RunAsync();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+                Host.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+}
