1ac4bbe8c9c23100f6310f10dbd9bed3a8313415
Julia Mattjus <julia.mattjus@sigma.se>
2022-04-15 20:28:17 +0200

    Create file service

--- src/Infrastructure/External/WebpageDumper.Infrastructure.External/Extensions/HttpClientExtensions.cs
+++ src/Infrastructure/External/WebpageDumper.Infrastructure.External/Extensions/HttpClientExtensions.cs
@@ -6,21 +6,4 @@ public static class HttpClientExtensions
     {
         return client.GetStreamAsync(uri);
     }
-
-    // public static async Task DownloadFileToPathAsync(this HttpClient client, Uri uri, string fileName, string path = null)
-    // {
-    //     using (var s = await client.GetStreamAsync(uri))
-    //     {
-    //         if (path != null)
-    //         {
-    //             Directory.CreateDirectory(path);
-    //             fileName = $"{path}/{fileName}";
-    //         }
-
-    //         using (var fs = new FileStream(fileName, FileMode.CreateNew))
-    //         {
-    //             await s.CopyToAsync(fs);
-    //         }
-    //     }
-    // }
 }
