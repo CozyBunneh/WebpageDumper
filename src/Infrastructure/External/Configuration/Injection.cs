f0cd63a48083ae73d2f51135a0a0a098c0af026e
Julia Mattjus <julia.mattjus@sigma.se>
2022-04-15 13:05:40 +0200

    Initial project setup and skeleton

---
+++ src/Infrastructure/External/Configuration/Injection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace WebpageDumper.Infrastructure.External.Configuration;
+
+public static class Injections
+{
+    public static IServiceCollection AddExternalServices(
+      this IServiceCollection services)
+    {
+        return services;
+    }
+}
