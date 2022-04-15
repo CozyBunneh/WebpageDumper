69739f54311b2f7471b439e2a3b23f0e9f27149a
Julia Mattjus <julia.mattjus@sigma.se>
2022-04-15 20:59:26 +0200

    Download index.html of a given page

--- src/Domain/WebpageDumper.Domain/Services/WebpageDumperService.cs
+++ src/Domain/WebpageDumper.Domain/Services/WebpageDumperService.cs
@@ -1,19 +1,30 @@
 using Microsoft.Extensions.Logging;
 using WebpageDumper.Domain.Abstract.Services;
+using WebpageDumper.Infrastructure.External.Abstract.Service;
+using WebpageDumper.Infrastructure.Persistence.Services;
 
 namespace WebpageDumper.Domain.Services;
 
 public class WebpageDumperService : IWebpageDumperService
 {
-    ILogger<WebpageDumperService> _logger;
+    private ILogger<WebpageDumperService> _logger;
+    private IWebService _webService;
+    private IFileService _fileService;
 
-    public WebpageDumperService(ILogger<WebpageDumperService> logger)
+    public WebpageDumperService(ILogger<WebpageDumperService> logger,
+                                IWebService webService,
+                                IFileService fileService)
     {
         _logger = logger;
+        _webService = webService;
+        _fileService = fileService;
     }
 
-    public Task DumpWebpage(Uri uri)
+    public async Task DumpWebpage(Uri uri)
     {
-        return Task.CompletedTask;
+        String pageName = uri.Host;
+
+        Stream indexStream = await _webService.GetFileStreamAsync(uri);
+        await _fileService.WriteFileToPath(indexStream, "index.html", pageName);
     }
 }
