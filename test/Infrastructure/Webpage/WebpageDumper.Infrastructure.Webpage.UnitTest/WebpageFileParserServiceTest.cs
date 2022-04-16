d7b3ed07bdf0a3a8fefbba0836025a4a220b95c1
Julia Mattjus <julia.mattjus@sigma.se>
2022-04-16 20:03:36 +0200

    Various fixes - Correcting missing naming change - Adding basic unit test project

---
+++ test/Infrastructure/Webpage/WebpageDumper.Infrastructure.Webpage.UnitTest/WebpageFileParserServiceTest.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace WebpageDumper.Infrastructure.Webpage.UnitTest;
+
+// [TestFixture]
+public class WebpageFileParserServiceTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
