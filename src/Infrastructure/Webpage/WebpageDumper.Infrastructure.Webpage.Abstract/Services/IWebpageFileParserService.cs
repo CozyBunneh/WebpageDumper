33a1aca34c1bc0f5b475effd2771352990e56146
Julia Mattjus <julia.mattjus@sigma.se>
2022-04-17 01:44:45 +0200

    Better naming for webpage parser service

--- src/Infrastructure/Webpage/WebpageDumper.Infrastructure.Webpage.Abstract/Services/IWebpageFileParserService.cs
+++
@@ -1,8 +0,0 @@
-using WebpageDumper.Infrastructure.Webpage.Abstract.Models;
-
-namespace WebpageDumper.Infrastructure.Webpage.Abstract.Services;
-
-public interface IWebpageFileParserService
-{
-    IList<WebpageResource> ParserFileForInternalLinks(Uri pageUri, String fileAsString);
-}
