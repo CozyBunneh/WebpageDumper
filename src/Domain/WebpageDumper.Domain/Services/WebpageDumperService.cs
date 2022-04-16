c735959e94c2113d2f95147aaf30a0481b68f31b
Julia Mattjus <julia.mattjus@sigma.se>
2022-04-17 01:53:17 +0200

    Get a list of resources from a given webpage

--- src/Domain/WebpageDumper.Domain/Services/WebpageDumperService.cs
+++ src/Domain/WebpageDumper.Domain/Services/WebpageDumperService.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Logging;
 using WebpageDumper.Domain.Abstract.Services;
 using WebpageDumper.Infrastructure.Persistence.Services;
 using WebpageDumper.Infrastructure.Webpage.Abstract.Service;
+using WebpageDumper.Infrastructure.Webpage.Abstract.Services;
 
 namespace WebpageDumper.Domain.Services;
 
@@ -9,22 +10,23 @@ public class WebpageDumperService : IWebpageDumperService
 {
     private ILogger<WebpageDumperService> _logger;
     private IWebService _webService;
+    private IWebpageParserService _webpageParserService;
     private IFileService _fileService;
 
     public WebpageDumperService(ILogger<WebpageDumperService> logger,
                                 IWebService webService,
+                                IWebpageParserService webpageParserService,
                                 IFileService fileService)
     {
         _logger = logger;
         _webService = webService;
+        _webpageParserService = webpageParserService;
         _fileService = fileService;
     }
 
     public async Task DumpWebpage(Uri uri)
     {
-        String pageName = uri.Host;
-
-        Stream indexStream = await _webService.GetFileStreamAsync(uri);
-        await _fileService.WriteFileToPath(indexStream, "index.html", pageName);
+        var indexPageAsString = await _webService.GetFileAsStringAsync(uri);
+        var webpageResources = _webpageParserService.ParseWebpageForResources(uri, indexPageAsString);
     }
 }
